a4870a979cf6a765955b847d9fd525e4456f73ea
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-04-08 21:19:18 +0800

    add show_menu function

--- main.py
+++ main.py
@@ -18,6 +18,37 @@ def search_record():
     # TODO (Kyle): implement record searching in this function.
     pass
 
+def show_menu():
+    print("---DATA RECORD MAIN MENU(MOVIES)---")
+    print("1. List All")
+    print("2. Add")
+    print("3. Update")
+    print("4. Delete")
+    print("5. Search")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+    choice = int(choice)
+
+    match choice:
+        case 1:
+            list_all_records()
+        case 2:
+            add_record()
+        case 3:
+            update_record()
+        case 4:
+            delete_record()
+        case 5:
+            search_record()
+        case 6:
+            # TODO (Miko): implement exit function here
+            pass
+        case _:
+            print("Invalid choice. Please try again.")
+
 def main():
     # TODO (everyone): Call your functions here
+
+    show_menu()
     pass
