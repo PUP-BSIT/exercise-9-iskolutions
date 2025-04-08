8135eaedc3be1f4ed5470a217bc426cdceba1eea
causon-mikolorenz <causonmikolorenz@gmail.com>
2025-04-08 21:04:25 +0800

    add delegations

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+def list_all_records():
+    # TODO (Pearl): implement listing all records in this funciton.
+    pass
+
+def add_record():
+    # TODO (Jakim): implement adding records in this function.
+    pass
+
+def update_record():
+    # TODO (Miko): implement updating record in this function.
+    pass
+
+def delete_record():
+    # TODO (Hanz): implement deleting records in this function.
+    pass
+
+def search_record():
+    # TODO (Kyle): implement record searching in this function.
+    pass
+
 def main():
     # TODO (everyone): Call your functions here
     pass
